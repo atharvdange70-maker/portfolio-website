4ccf5980d24d6d0949b27469919be4ebd1cff049
atharvdange70-maker <atharvdange70@gmail.com>
2026-07-11 16:10:47 +0530

    modified:   components/Stats.tsx

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -23,50 +23,51 @@ const stats = [
 
 export default function Stats() {
   return (
-    <section className="bg-[#111315] py-16">
-      <div className="max-w-7xl mx-auto px-6">
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+    <section className="bg-[#111315] py-14">
+      <div className="max-w-6xl mx-auto px-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
 
           {stats.map((item, index) => (
             <motion.div
               key={index}
-              whileHover={{ y: -6, scale: 1.03 }}
-              transition={{ duration: 0.3 }}
+              whileHover={{
+                y: -5,
+                scale: 1.02,
+              }}
+              transition={{ duration: 0.25 }}
               className="group relative"
             >
               {/* Glow */}
-              <div className="absolute inset-0 rounded-[26px] bg-sky-500/10 blur-2xl opacity-0 group-hover:opacity-100 transition duration-500"></div>
+              <div className="absolute inset-0 rounded-[22px] bg-sky-500/10 blur-xl opacity-0 group-hover:opacity-100 transition-all duration-300" />
 
               {/* Card */}
               <div
                 className="
                   relative
-                  h-35
-                  sm:h-36.25
-                  lg:h-37.5
-                  rounded-[26px]
+                  h-30
+                  md:h-32.5
+                  rounded-[22px]
                   border
-                  border-sky-500/25
+                  border-sky-500/20
                   bg-linear-to-br
-                  from-[#23272c]
+                  from-[#23272b]
                   to-[#1b1d21]
                   flex
                   flex-col
                   items-center
                   justify-center
                   text-center
-                  shadow-[0_15px_40px_rgba(0,0,0,0.45)]
+                  shadow-[0_12px_30px_rgba(0,0,0,0.45)]
                   transition-all
                   duration-300
                   group-hover:border-sky-400/40
                 "
               >
-                <h2 className="text-4xl md:text-5xl font-bold text-sky-400 leading-none">
+                <h2 className="text-4xl md:text-[42px] font-bold text-sky-400 leading-none">
                   {item.number}
                 </h2>
 
-                <p className="mt-5 text-[17px] md:text-xl text-gray-300">
+                <p className="mt-3 text-[15px] md:text-[18px] text-gray-300">
                   {item.title}
                 </p>
               </div>
@@ -74,7 +75,6 @@ export default function Stats() {
           ))}
 
         </div>
-
       </div>
     </section>
   );
