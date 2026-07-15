05bc17aa2a8da067cd3a8f68aa84c16355a08594
atharvdange70-maker <atharvdange70@gmail.com>
2026-07-15 09:58:41 +0530

    modified:    components/cloud/Scene.tsx

--- components/cloud/Scene.tsx
+++ components/cloud/Scene.tsx
@@ -1,22 +1,42 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
+
+function Cube() {
+  const cubeRef = useRef<THREE.Mesh>(null!);
+
+  useFrame(() => {
+    cubeRef.current.rotation.x += 0.01;
+    cubeRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={cubeRef}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color="#00e5ff" />
+    </mesh>
+  );
+}
 
 export default function Scene() {
   return (
     <Canvas
-      camera={{ position: [0, 0, 8] }}
+      camera={{ position: [0, 0, 6], fov: 60 }}
       style={{
         position: "absolute",
         inset: 0,
       }}
     >
-      <ambientLight intensity={2} />
+      <ambientLight intensity={1.5} />
 
-      <mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial color="cyan" />
-      </mesh>
+      <directionalLight
+        position={[5, 5, 5]}
+        intensity={3}
+      />
+
+      <Cube />
     </Canvas>
   );
 }
