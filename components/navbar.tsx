618c97ec20916381030ec2a1038524cb5f9ec80f
atharvdange70-maker <atharvdange70@gmail.com>
2026-07-16 00:41:39 +0530

    modified:  components/navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-slate-950/90 backdrop-blur z-50">
       <div className="max-w-7xl mx-auto px-6 py-3 flex justify-between items-center">
-        <h1 className="text-xl font-bold text-blue-400">
+        <h1 className="text-xl font-bold text-white-400">
           Atharv Dange
         </h1>
 
